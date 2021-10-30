829b7dd4fc61db070abf3160e7318f89cb89c847
Du-devBR <89052479+Du-devBR@users.noreply.github.com>
2021-10-30 17:16:51 -0300

    first commit new branch

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 
 const linkSocialMedia = {
 
-  github: "Du-devBR",
+  github: prompt("Digite seu id do Github"),
   youtube: "channel/UCOjh2L-G4nE69z6yR2D7cFA",
   facebook: "Eduardo Ananias da Silva",
-  instagram: "eduaro.bills",
+  instagram: prompt("Digite seu login do Instagram"),
   twitter: "twitter"
 
 }
@@ -20,3 +20,20 @@ function changeLinkSocialMedia() {
 }
 
 changeLinkSocialMedia()
+
+function getInfoGithub() {
+  const url = `https://api.github.com/users/${linkSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      userImage.src = data.avatar_url;
+      userLink.textContent = data.login
+    })
+}
+
+
+getInfoGithub()
